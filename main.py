0d261c720d8b884f0e95ca7dc7f18b16b14c2062
Patrick Lippert <patrick.lippert1@gmail.com>
2017-07-05 21:52:07 -0700

    I have no idea

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True  
+
+@app.route("/")
+def index():
+    return render_template('info.html')
+
+app.run()

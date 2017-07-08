62e5f28c3737346b08595793ada3cb2440dec237
Patrick Lippert <patrick.lippert1@gmail.com>
2017-07-07 17:38:57 -0700

    fixed broken content, renamed files to fit template format

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True  
 
+@app.route("/enter", methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify']
+    email = request.form['email']
+
+    return render_template('welcome.html',username=username)
+
+
 @app.route("/")
 def index():
-    return render_template('info.html')
+    return render_template('signup.html',title="Signup")
 
 app.run()
